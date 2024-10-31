f45ba1cb71fc8d22662b1fc76da2bb22a2906809
Dobeeee <wsh0684@naver.com>
2024-10-31 12:44:06 +0900

    plus

--- src/main/java/com/dostin/exam/sbb/HomeController.java
+++ src/main/java/com/dostin/exam/sbb/HomeController.java
@@ -2,7 +2,9 @@ package com.dostin.exam.sbb;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //  @Controller : 스프링부트한테 이 클래스는 컨트롤러 역할이라고 알려준다.
@@ -12,6 +14,12 @@ public class HomeController {
     // @ResponseBody : 함수 실행 결과를 body에 그려준다.
     @ResponseBody
     public String showHome() {
-        return "첫 시작";
+        return "라이브";
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam(defaultValue = "3") int a, @RequestParam(defaultValue = "1") int b) {
+        return a + b;
     }
 }
