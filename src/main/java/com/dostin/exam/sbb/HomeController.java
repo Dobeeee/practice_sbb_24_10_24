ee8b46d0d021df6be7549812e75f5872a6b7a08d
Dobeeee <wsh0684@naver.com>
2024-11-07 17:29:19 +0900

    게시물 상세보기

--- src/main/java/com/dostin/exam/sbb/HomeController.java
+++ src/main/java/com/dostin/exam/sbb/HomeController.java
@@ -92,6 +92,16 @@ public class HomeController {
 
     }
 
+    @GetMapping("/article/detail/{id}")
+    @ResponseBody
+    public Article getArticle(@PathVariable int id) {
+        Article article = articles.stream()
+                .filter(a -> a.getId() == id) //게시물 id 와 내가 입력한 id가 일치한지 확인
+                .findFirst()
+                .orElse(null); // 입력한 번호의 게시물이 없으면 null 반환
+        return article;
+    }
+
 
 }
 
